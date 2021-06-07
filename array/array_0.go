d8857538a3051162eae2bb9ae586cddc3823ab7d
fran <1317932489@qq.com>
2021-06-08 00:14:26 +0800

    move zero

--- array/array_0.go
+++ array/array_0.go
@@ -1,5 +1,24 @@
 package array
 
 func MoveZeroes(nums []int){
-
+	//找到非0需要填充到位置
+	
+	//for i:=0;i< len(nums);i++{
+	//	if nums[i]!=0{
+	//		tmp:= nums[j]
+	//		nums[j] = nums[i]
+	//		nums[i] = tmp
+	//		j++
+	//	}
+	//}
+	
+	for i,j:=0,0;i< len(nums);i++{
+		if nums[i]!=0{
+			if(i!=j){
+				nums[j]=nums[i]
+				nums[i]=0
+			}
+			j++
+		}
+	}
 }
